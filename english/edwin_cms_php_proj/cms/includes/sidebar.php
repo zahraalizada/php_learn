43449167c0244cfae6b7c1963b6a4429e8a5aba0
Zehra Alizada <zehra.alizada@ibp.az>
2023-04-17 17:58:03 +0400

    fff

--- english/edwin_cms_php_proj/cms/includes/sidebar.php
+++ english/edwin_cms_php_proj/cms/includes/sidebar.php
@@ -1,15 +1,16 @@
 <div class="col-md-4">
 
-    <!-- Blog Search Well -->
+    <!-- Login -->
     <div class="well">
-        <h4>Blog Search</h4>
-        <form action="search.php" method="post">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+                <div class="form-group   ">
+                <input name="username" type="text" class="form-control" placeholder="Enter Username">
+            </div>
             <div class="input-group">
-                <input name="search" type="text" class="form-control">
+                <input name="password" type="password" class="form-control" placeholder="Enter Password">
                 <span class="input-group-btn">
-                    <button name="submit" class="btn btn-default" type="submit">
-                        <span class="glyphicon glyphicon-search"></span>
-                    </button>
+                    <button class="btn btn-primary" name="login" type="submit"> Log in  </button>
                 </span>
             </div>
         </form>
