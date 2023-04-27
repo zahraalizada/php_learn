9596b7ffbb20411a878335121bd8db7df2b0dd81
Zehra Alizada <zehra.alizada@ibp.az>
2023-04-27 19:27:50 +0400

    ff

--- english/edwin_cms_php_proj/cms/index-old.php
+++ english/edwin_cms_php_proj/cms/index-old.php
@@ -13,36 +13,8 @@
         <div class="col-md-8">
 
             <?php
-
-
-            if(isset($_GET['page'])){
-
-                $per_page = 2;
-
-                $page = $_GET['page'];
-            } else{
-                $page = "";
-            }
-
-            if($page == "" || $page == 1){
-                $page_1 = 0;
-            } else{
-                $page_1 = ($page * $per_page) - $per_page;
-            }
-
-
-
-
-            $post_query_count = "SELECT * FROM posts";
-            $find_count = mysqli_query($connection, $post_query_count);
-            $count = mysqli_num_rows($find_count);
-            $count = ceil($count / $per_page);
-
-
-
-
             // posts table-dan hersey secilir,
-            $query = "SELECT * FROM posts LIMIT $page_1, $per_page";
+            $query = "SELECT * FROM posts ";
             // db -ile elaqe qurulur table arasinda
             $select_all_posts_query = mysqli_query($connection, $query);
 
@@ -71,7 +43,6 @@
                     </h1>
 
                     <!-- First Blog Post -->
-                    <h1 class="text-danger"><?php echo $count; ?></h1>
                     <h2>
                         <a href="post.php?p_id=<?php echo $post_id; // dinamic datalar ucun deyisken yazilir ?> "><?php echo $post_title; // dinamic datalar ucun deyisken yazilir ?> </a>
                     </h2>
@@ -103,29 +74,5 @@
     <!-- /.row -->
 
     <hr>
-
-
-    <ul class="pager">
-
-        <?php
-            for($i=1; $i<=$count;$i++){
-
-                if($i == $page ){
-
-                    echo "<li><a class='active_link' href='index.php?page={$i}'>$i</a></li>";
-
-                } else{
-                    echo "<li><a href='index.php?page={$i}'>$i</a></li>";
-                }
-
-
-
-
-            }
-
-        ?>
-    </ul>
-
-
 
 <?php include "includes/footer.php"; ?>
---
+++ turkish/yavuz_phppdodersleri/ajax_veriekle.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax ile veriekleme</title>
+
+    <!--  Bootstrap Css  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+
+
+<!-- Jquery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
+        integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!--Bootstrap Js-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+
+</body>
+</html>
